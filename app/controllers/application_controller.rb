d0b712a30a5beb188d0b57003344d4be2739d39c
Charlie Whight <cdwhight@gmail.com>
2020-03-17 09:33:44 +0000

    ashjkd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,28 @@
 class ApplicationController < ActionController::Base
-
+  before_action :authenticate_user!, except: [:new, :congratulations]
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   # Pundit: white-list approach.
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+
+    current_user.employer ? redirect_to(users_path) : redirect_to(jobs_path)
+  end
+
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :surname, :dob, :summary, :photo, :employer])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :surname, :dob, :summary, :photo, :employer])
+  end
 
   private
 
