9206ea5ceb0da76419c664b7467eeafa91f86d6d
Chris Williams <chriswilliams14.williams@gmail.com>
2020-03-20 14:34:25 +0000

    Few small changes to Navbar

--- db/schema.rb
+++ db/schema.rb
@@ -196,6 +196,9 @@ ActiveRecord::Schema.define(version: 2020_03_17_091314) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -206,6 +209,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_091314) do
     t.text "summary"
     t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
