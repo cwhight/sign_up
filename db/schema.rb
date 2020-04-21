696105ece37f67004622617595673be3586877e0
Charlie Whight <cdwhight@gmail.com>
2020-04-21 07:56:11 +0100

    fix bottom

--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_123023) do
     t.boolean "done"
     t.string "summary"
     t.text "dress"
+    t.string "venue"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
@@ -197,6 +198,9 @@ ActiveRecord::Schema.define(version: 2020_04_20_123023) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -207,6 +211,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_123023) do
     t.text "summary"
     t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
