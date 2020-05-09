33d6051a7d10ad12644db227d71abba1e3a6ce35
Charlie Whight <cdwhight@gmail.com>
2020-05-09 17:32:18 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_095614) do
+ActiveRecord::Schema.define(version: 2020_04_20_123023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,8 +68,6 @@ ActiveRecord::Schema.define(version: 2020_05_06_095614) do
     t.boolean "done"
     t.string "summary"
     t.text "dress"
-    t.string "venue"
-    t.float "pay"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
@@ -199,9 +197,6 @@ ActiveRecord::Schema.define(version: 2020_05_06_095614) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -211,9 +206,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_095614) do
     t.boolean "employer", default: false
     t.text "summary"
     t.boolean "admin", default: false, null: false
-    t.float "review_score"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
